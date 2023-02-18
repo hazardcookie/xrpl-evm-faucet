b89b22e7e7020d90d6769f7ffbe0091316ec52c7
hazaredcookie <wwallace@ripple.com>
2023-02-18 12:58:02 -0500

    autofunder

--- src/bridge.ts
+++ src/bridge.ts
@@ -1,25 +1,39 @@
-import { sign_submit_await } from './submit'
-import { Wallet, xrpToDrops, Client, Payment } from 'xrpl'
+import { Mapped_Keys } from './types'
+import { bridge } from './transaction'
+import { map_xrpl_secret_to_evm } from './mapping'
+import { AccountTxRequest, Client, Wallet } from 'xrpl'
+import { generateFundedWallet } from '@thebettermint/xrpl-auto-funder'
 
-export async function bridge(client: Client, xrpl_wallet: Wallet, evm_wallet_address: string, amount: number) {
-  const transaction: Payment = {
-    TransactionType: 'Payment',
-    Account: xrpl_wallet.address,
-    Amount: xrpToDrops(amount),
-    Destination: 'radjmEZTb4zsyNUJGV4gcVPXrFTJAuskKa',
-    Memos: [
-      {
-        Memo: {
-          MemoData: Buffer.from(evm_wallet_address, 'utf8').toString('hex').toUpperCase()
-        }
-      }
-    ]
-  }
-
-  const result = await sign_submit_await(client, transaction, xrpl_wallet)
-  if (result.TransactionResult === 'tesSUCCESS') {
-    console.log(`View your mapped EVM sidechain wallet: https://evm-sidechain.xrpl.org/address/${evm_wallet_address}`)
-  }
-
-  return result
+export const wait = async (time: number) => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, time)
+  })
 }
+
+/// @author: hazardcookie
+async function main() {
+  const client: Client = new Client('wss://s.devnet.rippletest.net:51233')
+  await client.connect()
+
+  const faucet = await generateFundedWallet('devnet')
+  const xrpl_wallet = Wallet.fromSeed(faucet.account.secret)
+  const evm_wallet = map_xrpl_secret_to_evm(faucet.account.secret) as Mapped_Keys
+  console.log(evm_wallet)
+
+  // Wait and check for fund wallet transaction to go through to receiving wallet
+  // Hack: if the number of transactions exceeds 0 (ie transactions.length===1), we can presumme that the funds have made it!
+  // If you check my fund wallet package, I had to do something similar, await for finality
+  // Opinion, 'create_wallet()' function should be a promise, and only returned when the funding transaction has been successfully sent from faucet. Or at least, give use a hash to check... lol
+  const request: AccountTxRequest = { command: 'account_tx', account: xrpl_wallet.address }
+  while (true) {
+    let r = await client.request(request)
+    await wait(500)
+    if (r.result.transactions.length > 0) break
+  }
+
+  const result = await bridge(client, xrpl_wallet, evm_wallet.mapped_evm_public_address, 100)
+  console.log(result)
+
+  client.disconnect()
+}
+main()
